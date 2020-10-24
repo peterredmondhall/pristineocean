d3972c06ca8b747919a54b90ac686cd803fbb953
peterredmondhall <hall@hall-services.de>
2020-10-24 15:32:23 +0200

    added build
    added build

---
+++ build/precache-manifest.220f6edea481148b82f72349526d5559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e22f19c3ee34632bb33046548af80e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bcd62800e1beb75f636",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "152c7b8b2ebe37a0c687",
+    "url": "/static/css/main.0231d968.chunk.css"
+  },
+  {
+    "revision": "2bcd62800e1beb75f636",
+    "url": "/static/js/2.0dacec71.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.0dacec71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152c7b8b2ebe37a0c687",
+    "url": "/static/js/main.97f7fc77.chunk.js"
+  },
+  {
+    "revision": "55dc2d6d8891bead9b41",
+    "url": "/static/js/runtime-main.31687faf.js"
+  }
+]);
